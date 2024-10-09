d41f8ba6894333c152ed73149170596c59cdc577
Sergio Lillo <sergiolillom@gmail.com>
2024-10-09 13:18:06 +0200

    First calculator in Python

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hola mundo");
+first_number = int(input('Type the first number: '));
+second_number = int(input('Type the second number: ')); 
+print("The sum is: ", first_number + second_number);
+
+#sadasd
