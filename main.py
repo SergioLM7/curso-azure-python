445a145b32f4228373fef3f2e91efa5a2511ce0c
Sergio Lillo <sergiolillom@gmail.com>
2024-10-09 13:27:11 +0200

    Calculator with timeout

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from os import system
+import time
+
+
 first_number = int(input('Type the first number: '));
 second_number = int(input('Type the second number: ')); 
 print("The sum is: ", first_number + second_number);
 
-#sadasd
+#Timeout 2s to clear the console
+time.sleep(2);
+system("cls");
+
+first_number = int(input('Type the first number: '));
+second_number = int(input('Type the second number: ')); 
+print("The dif is: ", first_number - second_number);
