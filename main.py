5cba3ecae09719852c1f1e15c12f2074b304e86f
Sergio Lillo <sergiolillom@gmail.com>
2024-10-09 13:51:31 +0200

    Exercise 1 Format parsecs into lightyears OK

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+#Fechas
 from datetime import date
 
 print(date.today())
 print("Today's date is: " + str(date.today().min))
 
+#Exercise 1 Convertion
 parsecs = 11
 lightyears = parsecs * 3.26
 print(F"{str(parsecs)} parsecs is {str(lightyears)} lightyears")
