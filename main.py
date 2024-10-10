dadce2e3096a8c435ef71031d6cde52ce995873a
Sergio Lillo <sergiolillom@gmail.com>
2024-10-10 12:04:17 +0200

    exercise 3 string templates

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-#Exercise String Methods
-text = "Interesting facts about the Moon. The Moon is Earth's only satellite. There are several interesting facts about the Moon and how it affects life here on Earth. On average, the Moon moves 4cm away from the Earth every year. This yearly drift is not significant enough to cause immediate effects on Earth. The highest daylight temperature of the Moon is 127 C."
-sentences = text.split('.');
-
-print(sentences)
-
-for sentence in sentences:
-    if "Moon" in sentence:
-        print(sentence.strip())
+#Exercise 3 String Templates
+name = "Ganymede"
+planet ="Mars"
+gravity = "1.43"
+template = f"Gravity Facts about {name}\n-------------------------- \nPlanet Name: {planet}\nGravity on {name}: {gravity} m/s2";
+print(template);
