5faf6002f9af2f28b7d29927e905cbfbdad4eccd
Sergio Lillo <sergiolillom@gmail.com>
2024-10-10 10:48:41 +0200

    first exercise with if/else and ternary operator

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-#Fechas
-from datetime import date
+# Exercise if/else Python 1 with and without Ternary Operator
+object_size = 10;
+#if object_size > 5:
+    #print("We need to keep an eye on this object");
+#else:
+    #print("Object poses no threat"); 
 
-print(date.today())
-print("Today's date is: " + str(date.today().min))
-
-#Exercise 1 Convertion
-parsecs = 11
-lightyears = parsecs * 3.26
-print(F"{str(parsecs)} parsecs is {str(lightyears)} lightyears")
+#Ternary Operator
+print("We need to keep an eye on this object") if object_size > 5 else print("Object poses no threat");
