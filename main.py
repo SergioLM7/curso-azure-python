fd7f0394f154dd1597fcea28ede7944c93378ae7
Sergio Lillo <sergiolillom@gmail.com>
2024-10-10 13:17:13 +0200

    Exercise 4 with conversions and whiles

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-#Exercise 3 String Templates
-name = "Ganymede"
-planet ="Mars"
-gravity = "1.43"
-template = f"Gravity Facts about {name}\n-------------------------- \nPlanet Name: {planet}\nGravity on {name}: {gravity} m/s2";
-print(template);
+#Exercise 4 inputs and conversions
+first_planet_input = input('Enter the distance from the sun for the first planet in km: ')
+
+while not first_planet_input.isnumeric():
+    first_planet_input = input('Enter the distance from the sun for the first planet in km: ')
+
+first_planet = int(first_planet_input);
+
+second_planet_input = input('Enter the distance from the sun for the second planet in km: ')
+while not second_planet_input.isnumeric():
+    second_planet_input = input('Enter the distance from the sun for the second planet in km: ')
+
+second_planet = int(second_planet_input);
+distance_km = abs(first_planet - second_planet);
+print(distance_km)
