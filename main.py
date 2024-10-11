8ff02eb55e237238003a0fa10557a8bd032a0a25
Sergio Lillo <sergiolillom@gmail.com>
2024-10-11 11:50:15 +0200

    Calculate the mean in Python

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-rainfall = {
-    'october': 3.5,
-    'november': 4.2,
-    'december': 2.1
+import numpy as np
+
+planet_moons = {
+"mercury": 0,
+"venus": 0,
+"earth": 1,
+"mars": 2,
+"jupiter": 79,
+"saturn": 82,
+"uranus": 27,
+"neptune": 14,
+"pluto": 5,
+"haumea": 2,
+"makemake": 1,
+"eris": 1
 }
 
-if 'december' in rainfall:
-    rainfall['december'] += 1
-else:
-    rainfall['december'] = 1
-
-for key, value in rainfall.items():
-    print(f'{key}: {value}cm')
-
-print(f'There was {sum(rainfall.values())}cm in the last quarter.')
+total_moons = sum(planet_moons.values());
+total_planets = len(planet_moons);
+#Sin biblioteca
+print(f'Each planet has an average of {total_moons / total_planets} moons')
+#Usando bibliotecta y convirtendola en una list
+print(f'Each planet has an average of {np.mean(list(planet_moons.values()))} moons')
