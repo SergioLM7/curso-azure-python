7b4a82d1b4065ebd538b8bcc0e1683b7eb1266ae
Sergio Lillo <sergiolillom@gmail.com>
2024-10-11 10:55:40 +0200

    Whiles in Python

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-#mercury_index = planets.index("Mercury")
-#print(planets)
-gravity_on_planets = [0.378, 0.907, 1, 0.377, 2.36, 0.916, 0.889, 1.12];
-bus_weight = 124054 # in Newtons, on Earth
+new_planet = ''
+planets = []
 
-#Devuelve el indice 
-for i in range(len(planets)):
-        print(f"On {planets[i]}, a double-decker bus weighs {int((bus_weight * gravity_on_planets[i])/9.81)}kg")
+while new_planet != "done":
+    if new_planet:
+        planets.append(new_planet)
+    new_planet = input("Enter a new planet name, or done if you just finished: ")
 
-
-print("On Earth, a double-decker bus weighs", int(bus_weight/9.81), "kg")
-#print("On Mercury, a double-decker bus weighs", bus_weight * gravity_on_planets[0], "N")
+print(planets)
