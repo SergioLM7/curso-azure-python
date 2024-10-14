7b13666c8ab10c2ffc963e034d755f58f549bc1f
Sergio Lillo <sergiolillom@gmail.com>
2024-10-14 12:33:10 +0200

    Exercise about Exceptions Control Python

---
+++ config.py
@@ -0,0 +1,15 @@
+#Exercise Exceptions Control
+loaded_config = """# Rocket Ship Configuration File!
+fuel_tanks=4
+oxygen_tanks=3
+initial_propulsion_level=84
+$ End of file"""
+parsed_config = {}
+for line in loaded_config.split("\n"):
+    try:
+        key,value = line.split("=");
+        parsed_config[key] = value;
+    except ValueError:
+        print(f"Value not valid: {line}")
+
+print(parsed_config)
