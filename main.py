7471d2fefe5f983641d293230d364dd16fd98f43
Sergio Lillo <sergiolillom@gmail.com>
2024-10-14 13:08:32 +0200

    Exercise exceptions improved OK

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ true_values = ['yes', 'y']
 false_values = ['no', 'n']
 
 def str_to_bool(value):
-    if(type(value) != "str"):
-       raise TypeError("Not supported type.")
+    if isinstance(value, str):
+       value = value.lower();
     else:
-        value = value.lower();
+        raise TypeError("Not supported type.")
     
     if value in true_values:
         return True;
@@ -16,8 +16,8 @@ def str_to_bool(value):
         raise ValueError("Not valid value.")
 
 try:
-    print(str_to_bool(1));
-except Exception as ex:
+    print(str_to_bool("False"));
+except TypeError as ex:
     print(ex);
 except Exception as e:
     print(e);
