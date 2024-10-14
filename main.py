519a9e7bfa0cdf77725064f7d8b87a87369335e9
Sergio Lillo <sergiolillom@gmail.com>
2024-10-14 13:05:26 +0200

    Exercise Exceptions improved in Python

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-#Errors to control exceptions. Raise exceptions inside the code. Like the throw in C#
-def water_left(astronauts, water_left, days_left):
-    for argument in [astronauts, water_left, days_left]:
-        try:
-            # If argument is an int, the following operation will work
-            argument / 10
-        except TypeError:
-            # TypeError will be raised only if it isn't the right type 
-            # Raise the same exception but with a better error message
-            raise RuntimeError(f"All arguments must be of type int, but received: '{argument}'")
-    daily_usage = astronauts * 11
-    total_usage = daily_usage * days_left
-    total_water_left = water_left - total_usage
-    if total_water_left < 0:
-        raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
-    return f"Total water left after {days_left} days is: {total_water_left} liters"
+#Exercise Exceptions in Python
+true_values = ['yes', 'y']
+false_values = ['no', 'n']
 
-def alert_navigation_system(error):
-    print(error);
+def str_to_bool(value):
+    if(type(value) != "str"):
+       raise TypeError("Not supported type.")
+    else:
+        value = value.lower();
+    
+    if value in true_values:
+        return True;
+    elif value in false_values:
+        return False;
+    else:
+        raise ValueError("Not valid value.")
 
 try:
-    print(water_left(10, 3.52432, "cosas"));
-except RuntimeError as err:
-    alert_navigation_system(err);
+    print(str_to_bool(1));
+except Exception as ex:
+    print(ex);
+except Exception as e:
+    print(e);
